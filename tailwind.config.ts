f5f3a390f1d8d21593b2add8dac61d2766f5f8d2
mingli202 <vincentmingli@gmail.com>
2025-01-25 18:45:02 -0500

    added tailwind custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        bg: "#f5ebe0",
+        fg: "#271a0c",
+        primary: "#d5bdaf",
+        secondary: "#e3d5ca",
+        accent: "#9ec58d",
+      },
+      fontFamily: {
+        header: ["var(--font-league-spartan)"],
+        text: ["var(--font-libre-baskerville)"],
+      },
+      fontSize: {
+        h1: "4.67rem",
+        h2: "4rem",
+        h3: "3.33rem",
+        h4: "2.67rem",
+        h5: "2rem",
+        h6: "1.67rem",
+        h7: "1.33rem",
       },
     },
   },
