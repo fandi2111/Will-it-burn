d111d55da49734907e0b84ce5527e69c8270d5ce
mingli202 <vincentmingli@gmail.com>
2025-01-26 08:52:38 -0500

    added a navbar thats just the title

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <div className="absolute left-0 top-0 z-20 flex w-screen justify-between overflow-hidden p-8 text-lg">
+      <div className="">Will It Burn</div>
+      {/*
+      <div className="flex gap-8">
+        <button>Contact</button>
+        <button>About</button>
+      </div>
+      */}
+    </div>
+  );
+}
