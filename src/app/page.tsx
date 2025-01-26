12a9601b3c9958eabf9851edae1c71db53b2aea1
mingli202 <vincentmingli@gmail.com>
2025-01-26 11:55:43 -0500

    omg plsss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
-      <h1 className="font-header text-h1">The landing page</h1>
+      <h1 className="font-header text-h1">Are You Aware?</h1>
       <div className="flex flex-col items-center justify-center gap-4">
         <div className="text-center font-text">
-          This will the be landing page when the user come.
+          Test your awareness level on social issues and learn more about them!
         </div>
         <Link href="/test">Begin</Link>
       </div>
